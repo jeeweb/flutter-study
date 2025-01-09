4030fac4e1c33388ce09db8ec855ac15bc5acfe4
jeeweb <52396673+jeeweb@users.noreply.github.com>
2025-01-10 00:49:51 +0900

    Feat: Practice with counter example

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -14,15 +14,35 @@ class App extends StatefulWidget {
 class _AppState extends State<App> {
   int counter = 0;
 
+  void onClicked() {
+    setState(() {
+      counter = counter + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Color(0xFFF4EDDB),
+        backgroundColor: const Color(0xFFF4EDDB),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [],
+            children: [
+              const Text(
+                'Click Count',
+                style: TextStyle(fontSize: 30),
+              ),
+              Text(
+                '$counter',
+                style: const TextStyle(fontSize: 30),
+              ),
+              IconButton(
+                iconSize: 40,
+                onPressed: onClicked,
+                icon: const Icon(Icons.add_box_rounded),
+              )
+            ],
           ),
         ),
       ),
