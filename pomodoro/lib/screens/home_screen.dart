c195e26845c80ea845b641643665ae635a84fd43
jeeweb <52396673+jeeweb@users.noreply.github.com>
2025-01-12 23:50:44 +0900

    init project

---
+++ pomodoro/lib/screens/home_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      body: Column(
+        children: [
+          Flexible(
+            flex: 1,
+            child: Container(),
+          ),
+          Flexible(
+            flex: 3,
+            child: Container(),
+          ),
+          Flexible(
+            flex: 1,
+            child: Container(),
+          ),
+        ],
+      ),
+    );
+  }
+}
