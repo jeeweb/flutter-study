dbbb8d085ae8ac824186fbf27d63de22d8a99139
jeeweb <52396673+jeeweb@users.noreply.github.com>
2025-01-14 12:08:23 +0900

    reset project

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -4,49 +4,13 @@ void main() {
   runApp(const App());
 }
 
-class App extends StatefulWidget {
+class App extends StatelessWidget {
   const App({super.key});
 
   @override
-  State<App> createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-          textTheme: const TextTheme(
-        titleLarge: TextStyle(color: Colors.red),
-      )),
-      home: const Scaffold(
-        backgroundColor: Color(0xFFF4EDDB),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              MyLargeTitle(),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class MyLargeTitle extends StatelessWidget {
-  const MyLargeTitle({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      'My Large Title',
-      style: TextStyle(
-        fontSize: 30,
-        color: Theme.of(context).textTheme.titleLarge?.color,
-      ),
+    return const MaterialApp(
+      home: Scaffold(),
     );
   }
 }
