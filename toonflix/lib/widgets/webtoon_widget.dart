a57deb22e35e2f05ae5b43a2c864226951586e4c
jeeweb <52396673+jeeweb@users.noreply.github.com>
2025-01-18 10:46:26 +0900

    Refactor: Separate webtoon widget

---
+++ toonflix/lib/widgets/webtoon_widget.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class Webtoon extends StatelessWidget {
+  final String title, thumb, id;
+
+  const Webtoon({
+    super.key,
+    required this.title,
+    required this.thumb,
+    required this.id,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          width: 250,
+          clipBehavior: Clip.hardEdge,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: const [
+              BoxShadow(
+                blurRadius: 10,
+                offset: Offset(4, 4),
+                color: Color.fromRGBO(0, 0, 0, 0.5),
+              ),
+            ],
+          ),
+          child: Image.network(
+            thumb,
+            headers: const {
+              "User-Agent":
+                  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+            },
+          ),
+        ),
+        const SizedBox(
+          height: 15,
+        ),
+        Text(
+          title,
+          style: const TextStyle(
+            fontSize: 22,
+          ),
+        ),
+      ],
+    );
+  }
+}
