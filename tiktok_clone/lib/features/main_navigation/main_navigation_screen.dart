1f3a4f4b9c980d6c93656d92ed37f72f191f9d17
jeeweb <52396673+jeeweb@users.noreply.github.com>
2025-01-29 14:50:01 +0900

    Feat: Add tab navigation - BottomNavigationBar

--- tiktok_clone/lib/features/main_navigation/main_navigation_screen.dart
+++ tiktok_clone/lib/features/main_navigation/main_navigation_screen.dart
@@ -1,10 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class MainNavigationScreen extends StatelessWidget {
+class MainNavigationScreen extends StatefulWidget {
   const MainNavigationScreen({super.key});
 
   @override
+  State<MainNavigationScreen> createState() => _MainNavigationScreenState();
+}
+
+class _MainNavigationScreenState extends State<MainNavigationScreen> {
+  int _selectedIndex = 0;
+
+  final screens = [
+    Center(
+      child: Text('Home'),
+    ),
+    Center(
+      child: Text('Search'),
+    ),
+  ];
+
+  void _onTab(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _onTab,
+        currentIndex: _selectedIndex,
+        selectedItemColor: Theme.of(context).primaryColor,
+        items: [
+          BottomNavigationBarItem(
+            icon: FaIcon(
+              FontAwesomeIcons.house,
+            ),
+            label: "Home",
+            tooltip: "What are you?",
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(
+              FontAwesomeIcons.magnifyingGlass,
+            ),
+            label: "Search",
+            tooltip: "Search",
+          ),
+        ],
+      ),
+    );
   }
 }
