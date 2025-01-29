b509cca6e37c15d955dc60c607da4e9a123035d4
jeeweb <52396673+jeeweb@users.noreply.github.com>
2025-01-29 17:14:54 +0900

    Feat: Practice CupertinoTabBar widget

--- tiktok_clone/lib/main.dart
+++ tiktok_clone/lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:flutter/material.dart';
-import 'package:tiktok_clone/constants/sizes.dart';
-import 'package:tiktok_clone/features/authentication/sign_up_screen.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:tiktok_clone/features/%08main_navigation/main_navigation_screen.dart';
 
 void main() {
   runApp(const TikTokApp());
@@ -11,24 +10,9 @@ class TikTokApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return CupertinoApp(
       title: 'TickTok Clone',
-      theme: ThemeData(
-        scaffoldBackgroundColor: Colors.white,
-        primaryColor: Color(0xFFE9435A),
-        appBarTheme: AppBarTheme(
-          foregroundColor: Colors.black,
-          backgroundColor: Colors.white,
-          elevation: 0,
-          titleTextStyle: TextStyle(
-            color: Colors.black,
-            fontSize: Sizes.size16 + Sizes.size2,
-            fontWeight: FontWeight.w600,
-          ),
-        ),
-        useMaterial3: false,
-      ),
-      home: SignUpScreen(),
+      home: MainNavigationScreen(),
     );
   }
 }
