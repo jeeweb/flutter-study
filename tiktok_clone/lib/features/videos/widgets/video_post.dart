c122f61e6d59e85eec063654d6024b9556274c81
jeeweb <52396673+jeeweb@users.noreply.github.com>
2025-02-01 23:44:29 +0900

    Feat: Add animation effect to play button with AnimationController

--- tiktok_clone/lib/features/videos/widgets/video_post.dart
+++ tiktok_clone/lib/features/videos/widgets/video_post.dart
@@ -1,18 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:video_player/video_player.dart';
+import 'package:visibility_detector/visibility_detector.dart';
 
 class VideoPost extends StatefulWidget {
   final Function onVideoFinished;
+  final int index;
 
-  const VideoPost({super.key, required this.onVideoFinished});
+  const VideoPost(
+      {super.key, required this.onVideoFinished, required this.index});
 
   @override
   State<VideoPost> createState() => _VideoPostState();
 }
 
-class _VideoPostState extends State<VideoPost> {
+class _VideoPostState extends State<VideoPost>
+    with SingleTickerProviderStateMixin {
   final VideoPlayerController _videoPlayerController =
       VideoPlayerController.asset("assets/videos/example.mp4");
+
+  late final AnimationController _animationController;
+
+  bool _isPaused = false;
+  final Duration _animationDuration = Duration(milliseconds: 300);
 
   void _onVideoChange() {
     if (_videoPlayerController.value.isInitialized) {
@@ -34,6 +45,19 @@ class _VideoPostState extends State<VideoPost> {
   void initState() {
     super.initState();
     _initVideoPlayer();
+
+    _animationController = AnimationController(
+      vsync: this,
+      lowerBound: 1.0,
+      upperBound: 1.5,
+      value: 1.5, // default
+      duration: _animationDuration,
+    );
+
+    _animationController.addListener(() {
+      //print(_animationController.value); // controller가 변하는 단계를 확인할 수 있음
+      setState(() {});
+    });
   }
 
   @override
@@ -44,18 +68,67 @@ class _VideoPostState extends State<VideoPost> {
     widget.onVideoFinished();
   }
 
+  void _onVisibilityChanged(VisibilityInfo info) {
+    // print("video: #${widget.index} is ${info.visibleFraction * 100}% visible"); // widget이 얼만큼 보이는지에 대한 수치를 퍼센트로 보여주기
+    if (info.visibleFraction == 1 && !_videoPlayerController.value.isPlaying) {
+      // 현재 보고있는 영상이 화면에 가득차있고, _videoPlayerController.value가 재생중이 아니라면 재생하기
+      _videoPlayerController.play();
+      // -> 현재 보고 있는 영상만 재생되고 다음 영상은 완전히 화면 위로 올라올때까지 재생되지 않음
+    }
+  }
+
+  void _togglePause() {
+    if (_videoPlayerController.value.isPlaying) {
+      _videoPlayerController.pause();
+      _animationController.reverse();
+    } else {
+      _videoPlayerController.play();
+      _animationController.forward();
+    }
+    setState(() {
+      _isPaused = !_isPaused;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Positioned.fill(
-          child: _videoPlayerController.value.isInitialized
-              ? VideoPlayer(_videoPlayerController)
-              : Container(
-                  color: Colors.black,
+    return VisibilityDetector(
+      key: Key("${widget.index}"),
+      onVisibilityChanged: _onVisibilityChanged,
+      child: Stack(
+        children: [
+          Positioned.fill(
+            child: _videoPlayerController.value.isInitialized
+                ? VideoPlayer(_videoPlayerController)
+                : Container(
+                    color: Colors.black,
+                  ),
+          ),
+          Positioned.fill(
+            child: GestureDetector(
+              onTap: _togglePause,
+            ),
+          ),
+          Positioned.fill(
+            child: IgnorePointer(
+              child: Center(
+                child: Transform.scale(
+                  scale: _animationController.value,
+                  child: AnimatedOpacity(
+                    opacity: _isPaused ? 1 : 0,
+                    duration: _animationDuration,
+                    child: FaIcon(
+                      FontAwesomeIcons.play,
+                      color: Colors.white,
+                      size: Sizes.size52,
+                    ),
+                  ),
                 ),
-        ),
-      ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
