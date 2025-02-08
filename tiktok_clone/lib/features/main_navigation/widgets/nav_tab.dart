01992a21b4d0c7d8ba71668f5f6cea9e2e762f33
jeeweb <52396673+jeeweb@users.noreply.github.com>
2025-02-08 17:35:14 +0900

    Design: Change navigation color in other screen

--- tiktok_clone/lib/features/main_navigation/widgets/nav_tab.dart
+++ tiktok_clone/lib/features/main_navigation/widgets/nav_tab.dart
@@ -3,20 +3,21 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 
 class NavTab extends StatelessWidget {
-  const NavTab({
-    super.key,
-    required this.text,
-    required this.isSelected,
-    required this.icon,
-    required this.selectedIcon,
-    required this.onTap,
-  });
+  const NavTab(
+      {super.key,
+      required this.text,
+      required this.isSelected,
+      required this.icon,
+      required this.selectedIcon,
+      required this.onTap,
+      required this.selectedIndex});
 
   final String text;
   final bool isSelected;
   final IconData icon;
   final IconData selectedIcon;
   final Function onTap;
+  final int selectedIndex;
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +25,7 @@ class NavTab extends StatelessWidget {
       child: GestureDetector(
         onTap: () => onTap(),
         child: Container(
-          color: Colors.transparent,
+          color: selectedIndex == 0 ? Colors.black : Colors.white,
           child: AnimatedOpacity(
             duration: Duration(
               milliseconds: 300,
@@ -35,13 +36,13 @@ class NavTab extends StatelessWidget {
               children: [
                 FaIcon(
                   isSelected ? selectedIcon : icon,
-                  color: Colors.white,
+                  color: selectedIndex == 0 ? Colors.white : Colors.black,
                 ),
                 Gaps.v5,
                 Text(
                   text,
                   style: TextStyle(
-                    color: Colors.white,
+                    color: selectedIndex == 0 ? Colors.white : Colors.black,
                   ),
                 ),
               ],
