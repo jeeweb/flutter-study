d28cbe7e3f921710f32467e955490baaa0d69d17
jeeweb <52396673+jeeweb@users.noreply.github.com>
2025-02-15 01:44:22 +0900

    Feat: Save video to gallery with Gal package

--- tiktok_clone/lib/features/videos/widgets/video_preview_screen.dart
+++ tiktok_clone/lib/features/videos/widgets/video_preview_screen.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:gal/gal.dart';
 import 'package:video_player/video_player.dart';
 
 class VideoPreviewScreen extends StatefulWidget {
@@ -15,6 +17,7 @@ class VideoPreviewScreen extends StatefulWidget {
 
 class _VideoPreviewScreenState extends State<VideoPreviewScreen> {
   late final VideoPlayerController _videoPlayerController;
+  bool _savedVideo = false;
 
   Future<void> _initVideo() async {
     _videoPlayerController = VideoPlayerController.file(
@@ -33,12 +36,29 @@ class _VideoPreviewScreenState extends State<VideoPreviewScreen> {
     _initVideo();
   }
 
+  Future<void> _saveToGallery() async {
+    if (_savedVideo) return;
+
+    await Gal.putVideo(widget.video.path);
+
+    _savedVideo = true;
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
         title: Text('Preview video'),
+        actions: [
+          IconButton(
+            onPressed: _saveToGallery,
+            icon: FaIcon(_savedVideo
+                ? FontAwesomeIcons.check
+                : FontAwesomeIcons.download),
+          )
+        ],
       ),
       body: _videoPlayerController.value.isInitialized
           ? VideoPlayer(_videoPlayerController)
