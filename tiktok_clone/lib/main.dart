8504d594d832b9da4019629fa71f982e8ab4e05d
jeeweb <52396673+jeeweb@users.noreply.github.com>
2025-02-16 16:56:26 +0900

    Design: Add dark mode theme

--- tiktok_clone/lib/main.dart
+++ tiktok_clone/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/authentication/sign_up_screen.dart';
 import 'package:tiktok_clone/features/main_navigation/main_navigation_screen.dart';
 
 void main() {
@@ -13,12 +14,17 @@ class TikTokApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'TickTok Clone',
+      themeMode: ThemeMode.light,
       theme: ThemeData(
+        brightness: Brightness.light, // 컬러를 별도로 지정하지 않은 텍스트들에 적용되는 기본 텍스트 컬러
         scaffoldBackgroundColor: Colors.white,
         primaryColor: Color(0xFFE9435A),
         textSelectionTheme: TextSelectionThemeData(
           cursorColor: Color(0xFFE9435A), // 커서 색상
           //selectionColor: Color(0xFFE9435A) // 드래그 블록 색상
+        ),
+        bottomAppBarTheme: BottomAppBarTheme(
+          color: Colors.grey.shade50,
         ),
         splashColor: Colors.transparent, // tab 할때 splash 효과 컬러
         highlightColor: Colors.transparent, // long press 할 때 효과 컬러
@@ -34,7 +40,15 @@ class TikTokApp extends StatelessWidget {
         ),
         useMaterial3: false,
       ),
-      home: MainNavigationScreen(),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: Colors.black,
+        primaryColor: Color(0xFFE9435A),
+        bottomAppBarTheme: BottomAppBarTheme(
+          color: Colors.grey.shade900,
+        ),
+      ),
+      home: SignUpScreen(),
     );
   }
 }
