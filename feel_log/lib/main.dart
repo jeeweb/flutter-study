acc39c50a74c594251ab3dd6a25ec807a5a9cfe8
jeeweb <52396673+jeeweb@users.noreply.github.com>
2025-03-02 22:13:24 +0900

    init project

---
+++ feel_log/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:feel_log/firebase_options.dart';
+import 'package:feel_log/router.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const FeelLogApp());
+}
+
+class FeelLogApp extends StatelessWidget {
+  const FeelLogApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routerConfig: router,
+      title: 'FeelLog',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+    );
+  }
+}
