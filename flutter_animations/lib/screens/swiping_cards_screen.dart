2ea8aa541fcd0c4f9127cec6b69b68b4d64ede30
jeeweb <52396673+jeeweb@users.noreply.github.com>
2025-03-21 00:01:34 +0900

    Feat: Infinite card swiping

--- flutter_animations/lib/screens/swiping_cards_screen.dart
+++ flutter_animations/lib/screens/swiping_cards_screen.dart
@@ -35,6 +35,13 @@ class _SwipingCardsScreenState extends State<SwipingCardsScreen>
     _animationController.value += details.delta.dx;
   }
 
+  void _whenComplete() {
+    _animationController.value = 0;
+    setState(() {
+      _index = _index == 4 ? 1 : _index + 1;
+    });
+  }
+
   void _onHorizontalDragEnd(DragEndDetails details) {
     // print(_animationController.value.abs());  // 카드가 움직인 값
     // print(size.width - 100);  // 카드를 사라지게 할 기준점. 해당 기준점보다 카드가 더 움직이면 사라지도록 하기
@@ -43,11 +50,19 @@ class _SwipingCardsScreenState extends State<SwipingCardsScreen>
     final dropZone = size.width + 200;
 
     if (_animationController.value.abs() >= bound) {
+      final factor = _animationController.value.isNegative ? -1 : 1;
+      _animationController
+          .animateTo(dropZone * factor, curve: Curves.bounceOut)
+          .whenComplete(_whenComplete);
+      /*
       if (_animationController.value.isNegative) {
-        _animationController.animateTo(dropZone * -1);
+        _animationController
+            .animateTo(dropZone * -1)
+            .whenComplete(_whenComplete);
       } else {
-        _animationController.animateTo(dropZone);
+        _animationController.animateTo(dropZone).whenComplete(_whenComplete);
       }
+      */
     } else {
       _animationController.animateTo(0, curve: Curves.bounceOut);
     }
@@ -58,6 +73,8 @@ class _SwipingCardsScreenState extends State<SwipingCardsScreen>
     _animationController.dispose();
     super.dispose();
   }
+
+  int _index = 1;
 
   @override
   Widget build(BuildContext context) {
@@ -79,15 +96,8 @@ class _SwipingCardsScreenState extends State<SwipingCardsScreen>
               Positioned(
                 top: 100,
                 child: Transform.scale(
-                  scale: scale,
-                  child: Material(
-                    elevation: 10,
-                    color: Colors.blue.shade100,
-                    child: SizedBox(
-                      width: size.width * 0.8,
-                      height: size.height * 0.5,
-                    ),
-                  ),
+                  scale: min(scale, 1.0),
+                  child: Card(index: _index == 4 ? 1 : _index + 1),
                 ),
               ),
               Positioned(
@@ -100,14 +110,7 @@ class _SwipingCardsScreenState extends State<SwipingCardsScreen>
                     offset: Offset(_animationController.value, 0),
                     child: Transform.rotate(
                       angle: angle * pi / 180,
-                      child: Material(
-                        elevation: 10,
-                        color: Colors.red.shade100,
-                        child: SizedBox(
-                          width: size.width * 0.8,
-                          height: size.height * 0.5,
-                        ),
-                      ),
+                      child: Card(index: _index),
                     ),
                   ),
                 ),
@@ -115,6 +118,30 @@ class _SwipingCardsScreenState extends State<SwipingCardsScreen>
             ],
           );
         },
+      ),
+    );
+  }
+}
+
+class Card extends StatelessWidget {
+  final int index;
+
+  const Card({super.key, required this.index});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Material(
+      elevation: 10,
+      borderRadius: BorderRadius.circular(10),
+      clipBehavior: Clip.hardEdge,
+      child: SizedBox(
+        width: size.width * 0.8,
+        height: size.height * 0.5,
+        child: Image.asset(
+          "assets/covers/$index.jpg",
+          fit: BoxFit.cover,
+        ),
       ),
     );
   }
