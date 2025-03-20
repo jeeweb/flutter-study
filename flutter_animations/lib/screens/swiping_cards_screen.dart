29091a84a0f63c74c898f9cc6b8b8354c997e692
jeeweb <52396673+jeeweb@users.noreply.github.com>
2025-03-20 21:05:50 +0900

    Refactor: Swiping animation with animation controller

--- flutter_animations/lib/screens/swiping_cards_screen.dart
+++ flutter_animations/lib/screens/swiping_cards_screen.dart
@@ -7,52 +7,65 @@ class SwipingCardsScreen extends StatefulWidget {
   State<SwipingCardsScreen> createState() => _SwipingCardsScreenState();
 }
 
-class _SwipingCardsScreenState extends State<SwipingCardsScreen> {
-  double posX = 0;
+class _SwipingCardsScreenState extends State<SwipingCardsScreen>
+    with SingleTickerProviderStateMixin {
+  late final size = MediaQuery.of(context).size;
+
+  late final AnimationController _animationController = AnimationController(
+    vsync: this,
+    duration: Duration(milliseconds: 2000),
+    lowerBound: size.width * -1,
+    upperBound: size.width,
+    value: 0.0,
+  );
 
   void _onHorizontalDragUpdate(DragUpdateDetails details) {
-    //print(details); // 카드를 드래그하면 Offset 정보가 콘솔에 표시됨
-    //print(details.delta.dx);  // 포인터가 움직인 양을 콘솔에 표기
-    setState(() {
-      posX += details.delta.dx; // 사용자가 드래그할 때 얻은 모든 변화량 누적
-    });
+    _animationController.value += details.delta.dx;
   }
 
   void _onHorizontalDragEnd(DragEndDetails details) {
-    setState(() {
-      posX = 0;
-    });
+    _animationController.animateTo(0, curve: Curves.bounceOut);
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: Text('Swiping Cards'),
       ),
-      body: Stack(
-        children: [
-          Align(
-            alignment: Alignment.topCenter,
-            child: GestureDetector(
-              onHorizontalDragUpdate: _onHorizontalDragUpdate,
-              onHorizontalDragEnd:
-                  _onHorizontalDragEnd, // 사용자가 카드를 가로로 드래그 할 때 호출
-              child: Transform.translate(
-                offset: Offset(posX, 0),
-                child: Material(
-                  elevation: 10,
-                  color: Colors.red.shade100,
-                  child: SizedBox(
-                    width: size.width * 0.8,
-                    height: size.height * 0.5,
+      body: AnimatedBuilder(
+        animation: _animationController,
+        builder: (context, child) {
+          return Stack(
+            children: [
+              Align(
+                alignment: Alignment.topCenter,
+                child: GestureDetector(
+                  onHorizontalDragUpdate: _onHorizontalDragUpdate,
+                  onHorizontalDragEnd:
+                      _onHorizontalDragEnd, // 사용자가 카드를 가로로 드래그 할 때 호출
+                  child: Transform.translate(
+                    offset: Offset(_animationController.value, 0),
+                    child: Material(
+                      elevation: 10,
+                      color: Colors.red.shade100,
+                      child: SizedBox(
+                        width: size.width * 0.8,
+                        height: size.height * 0.5,
+                      ),
+                    ),
                   ),
                 ),
-              ),
-            ),
-          )
-        ],
+              )
+            ],
+          );
+        },
       ),
     );
   }
